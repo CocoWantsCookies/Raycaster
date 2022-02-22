d0fb1eb71074f8635b02dfbd04afd8a62dcd0111
CocoWantsCookies <cocowantscookies@gmail.com>
2022-02-22 15:15:38 +0100

    Creating a default game screen

--- raycaster.py
+++ raycaster.py
@@ -1,1 +1,12 @@
-import pygame *
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+SCREEN = pygame.display.set_mode((300,300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
