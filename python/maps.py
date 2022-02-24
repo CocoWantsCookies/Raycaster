1594e0c885bba0cb9d023353306e88728fcb969a
CocoWantsCookies <cocowantscookies@gmail.com>
2022-02-24 21:34:55 +0100

    A lot of changes

--- python/maps.py
+++ python/maps.py
@@ -1,7 +1,23 @@
-game_map = [
-    [1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1],
-    [1, 1, 1, 1, 1]
-]
+#Creating a world class
+class world:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.Map = [[0 for x in range(width)] for y in range(height)]
+
+
+#Creating the levels
+    #Test Level
+worldTest = world(10, 8)
+for y in range (8):
+    for x in range (10):
+        worldTest.Map[y][x] = 1
+for y in range (1, worldTest.height-1):
+    for x in range (1, worldTest.width-1):
+        worldTest.Map[y][x] = 0
+
+
+#Functions
+def setWorld(Id):
+    if Id == 0:
+        return (worldTest)
--- python/raycaster.py
+++ python/raycaster.py
@@ -1,19 +1,52 @@
+#Imports
 import pygame
+import pygame.gfxdraw
 from pygame.locals import *
 
-import maps
+from maps import setWorld
 
-pygame.init()
 
-SCREEN = pygame.display.set_mode((256,224))
-pygame.display.set_caption('Raycaster')
-
+#Colors
+C_BLACK= pygame.Color(0, 0, 0)
 C_WHITE= pygame.Color(255, 255, 255)
+C_GREY= pygame.Color(150, 150, 150)
 
-pygame.display.update()
 
-# while True:
-#     for event in pygame.event.get():
-#         if event.type == QUIT:
-#             pygame.quit()
-#             sys.exit()
+#Window Init
+zoom = 3
+pygame.init()
+pygame.display.set_caption('Raycaster')
+SCREEN = pygame.display.set_mode((128*zoom,128*zoom))
+
+
+#Functions
+def draw():
+    SCREEN.fill(C_BLACK)
+    minimap()
+    pygame.display.update()
+
+def pixel(x, y, color):
+    for i in range (zoom):
+        for j in range (zoom):
+            pygame.gfxdraw.pixel(SCREEN, x*zoom+i, y*zoom+j, color)
+
+def minimap():
+    for y in range (world.height):
+        for x in range (world.width):
+            if world.Map[y][x] == 1:
+                pixel(x, y, C_GREY)
+            elif world.Map[y][x] == 0:
+                pixel(x, y, C_WHITE)
+
+
+#Game Init
+world = setWorld(0)
+
+
+#Game loop
+while True:
+    draw()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
